e3f5e6caa0fdf7f747eae70802fa4df890e34414
matzgans <rahmatlahay84@gmail.com>
2024-10-23 17:35:21 +0800

    First Setup : Slicing Landing Home

---
+++ resources/views/layouts/landing/navigation.blade.php
@@ -0,0 +1,42 @@
+<nav class="border-gray-200 bg-primary dark:bg-primary">
+    <div class="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
+        <a class="flex items-center space-x-3 rtl:space-x-reverse" href="https://flowbite.com/">
+            <h1 class="text-3xl font-extrabold text-white">SIDEPO</h1>
+        </a>
+        <button
+            class="dark:hover:bg-primary-dark dark:focus:ring-primary-dark inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-white md:hidden"
+            data-collapse-toggle="navbar-default" type="button" aria-controls="navbar-default" aria-expanded="false">
+            <span class="sr-only">Open main menu</span>
+            <svg class="h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 17 14">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    d="M1 1h15M1 7h15M1 13h15" />
+            </svg>
+        </button>
+        <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+            <ul
+                class="dark:border-primary-dark mt-4 flex flex-col rounded-lg border p-4 font-bold dark:bg-primary md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0">
+                <li>
+                    <a class="block rounded px-3 py-2 text-white md:bg-transparent md:p-0" href="#"
+                        aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a class="dark:hover:bg-primary-dark block rounded px-3 py-2 text-white hover:text-primary_hover dark:text-primary md:border-0 md:p-0 md:hover:bg-transparent"
+                        href="#">About</a>
+                </li>
+                <li>
+                    <a class="dark:hover:bg-primary-dark block rounded px-3 py-2 text-white hover:text-primary_hover dark:text-primary md:border-0 md:p-0 md:hover:bg-transparent"
+                        href="#">Services</a>
+                </li>
+                <li>
+                    <a class="dark:hover:bg-primary-dark block rounded px-3 py-2 text-white hover:text-primary_hover dark:text-primary md:border-0 md:p-0 md:hover:bg-transparent"
+                        href="#">Pricing</a>
+                </li>
+                <li>
+                    <a class="dark:hover:bg-primary-dark block rounded px-3 py-2 text-white hover:text-primary_hover dark:text-primary md:border-0 md:p-0 md:hover:bg-transparent"
+                        href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
